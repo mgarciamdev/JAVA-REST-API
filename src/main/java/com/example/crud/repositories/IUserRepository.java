d086e6f39387b478e466f36ebce002cc5a69b2f7
mgarciamdev <mgarciadev117@gmail.com>
2024-01-29 16:51:33 -0600

    actualizacion de los archivos en carpetas repositories y services

--- src/main/java/com/example/crud/repositories/IUserRepository.java
+++ src/main/java/com/example/crud/repositories/IUserRepository.java
@@ -1,4 +1,9 @@
 package com.example.crud.repositories;
 
-public interface IUserRepository {
+import com.example.crud.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<UserModel, Long> {
 }
