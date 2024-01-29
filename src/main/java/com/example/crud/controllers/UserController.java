f39e38b87cc1f78274ae2035ec1155ccab8caba0
mgarciamdev <mgarciadev117@gmail.com>
2024-01-29 17:33:47 -0600

    se agrega nuevo archivo en la carpeta controllers

--- src/main/java/com/example/crud/controllers/UserController.java
+++ src/main/java/com/example/crud/controllers/UserController.java
@@ -1,4 +1,49 @@
 package com.example.crud.controllers;
 
+import com.example.crud.models.UserModel;
+import com.example.crud.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ArrayList<UserModel> getUsers() {
+        return this.userService.getUsers();
+    }
+
+    @PostMapping
+    public UserModel saveUser(@RequestBody UserModel user){
+        return this.userService.saveUser(user);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<UserModel> getUserById(@PathVariable Long id){
+        return this.userService.getById(id);
+    }
+
+    @PutMapping(path = "/{id}")
+    public UserModel updateUserBy(@RequestBody UserModel request, Long id){
+        return this.userService.updateById(request, id);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteById(@PathVariable("id") Long id){
+        boolean ok = this.userService.deleteUser(id);
+
+        if (ok){
+            return "Usuario con el id =" + id + "fue eliminado exitosamente";
+        } else {
+            return "Ocurrio un problema interno del servidor y no se pudo eliminar a el usuario con el id =" + id;
+        }
+    }
 }
