dbe29bf3e0521f29c85a87a1327693c9529c3152
mgarciamdev <mgarciadev117@gmail.com>
2024-01-29 16:28:25 -0600

    Se agregaron los nuevos archivos y se completo el userModel

---
+++ src/main/java/com/example/crud/models/UserModel.java
@@ -0,0 +1,53 @@
+package com.example.crud.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/example/crud/repositories/IUserRepository.java
@@ -0,0 +1,4 @@
+package com.example.crud.repositories;
+
+public interface IUserRepository {
+}
